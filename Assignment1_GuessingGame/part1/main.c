8de424437b1394ecce4280f9cf20783de4a4adf2
Eileen Chang <eilccn@login-students.ccs.neu.edu>
2021-01-30 05:17:51 -0500

    edit 3

--- Assignment1_GuessingGame/part1/main.c
+++ Assignment1_GuessingGame/part1/main.c
@@ -1,6 +1,6 @@
 // Guessing Game in C
-// param1:
-// expected output:
+// input: user inputs a number 1-10 until they guess the correct number. they will do this until they have completed 5 games. 
+// expected output: at the end of 5 games user is given a summary of how many guesses they made in each game.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,7 +30,7 @@ int playGame(int number){
 }
 
 int main(){
-	int i, number, current_guess, number_of_guesses;
+	int i, number, current_guess, num_guesses;
 	
 	srand((unsigned)time(NULL));	
 
@@ -38,12 +38,19 @@ int main(){
 		
 		printf("***************************\n");
 		printf("CPU Says: Pick a number 1-10 \n");
-		printf("***************************\n"); 
+		printf("***************************\n");
+ 
 		number = rand()%10+1;	
-		
-		playGame(number);
-	}	
+		num_guesses = playGame(number);
+	}
 	
+	printf("***************************\n");
+	printf("Here are your results!\n");
+	printf("***************************\n");
+	for (i=0; i<5; i++){
+		printf("Game %d took you %d guesses\n", i,
+		num_guesses);	
+	}
 	return 0;	
 
 
