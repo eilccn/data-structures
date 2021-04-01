0f2836efd927ed8e0440ec2f192d95cd595e9e65
eilccn <chng.eiln@gmail.com>
2021-04-01 18:03:34 -0400

    edit

--- Lab9_QuicklySorting/radio.c
+++ Lab9_QuicklySorting/radio.c
@@ -71,11 +71,27 @@ void bruteForceSort(char** array, unsigned int start, unsigned int end){
 // @Name    partition
 // @Brief   Helper funcion for quicksort
 int partition(char** array, unsigned int low, unsigned int high){
-    // TODO:
+	char** pivot = &array[high];
+
+	int i = (low-1); // Index of smaller element
+
+	for (int j = low; j <= high-1; j++){
+		if (&array[j] <= pivot){
+			i++;
+			swapStrings(&array[i], &array[j]);
+		}
+	}
+	swapStrings(&array[i +1], &array[high]);
+	return (i+1);			
 }
 
 void quicksort(char** array, unsigned int low, unsigned int high){
-    // TODO:
+	if (low<high){
+		int pivot = partition(array, low, high);
+
+		quicksort(array, low, pivot-1);
+		quicksort(array, pivot+1, high);
+	}
 }
 
 
