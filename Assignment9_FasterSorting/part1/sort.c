85926050c2f1211c7bb44c56507133c23150b152
eilccn <chng.eiln@gmail.com>
2021-04-03 01:25:55 -0400

    edit

--- Assignment9_FasterSorting/part1/sort.c
+++ Assignment9_FasterSorting/part1/sort.c
@@ -9,11 +9,65 @@
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
 void merge(int arr[], int l, int m, int r) {
-  //TODO: implement merge.
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	// Temp arrays
+	int L[n1], R[n2];
+
+	// Copy data to temp arrays L[] and R[]
+	for (i=0; i<n1; i++){
+		L[i] = arr[l + i];
+	}
+	for (j=0; j<n2; j++){
+		R[j] = arr[m + 1 + j];
+	}
+
+	// Merge temp arrays back
+	i = 0;
+	j = 0;
+	k = l;
+
+	while (i<n1 && j<n2){
+		if (L[i] <= R[j]){
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copy remaining elements of L[]
+	while (i<n1){
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	// Copy remaining elements of R[]
+	while (j<n2){
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
 }
 
 // Implement your mergeSort function here
-// TODO:
+void mergeSort(int arr[], int l, int r){
+	if (l<r){
+	
+		int m = l + (r-l) / 2;
+
+        	mergeSort(arr, l, m);
+        	mergeSort(arr, m+1, r);
+        	
+		merge(arr, l, m, r);
+	}
+}
 
 // Provided below is a sort function. We have also
 // provided a template for how to document functions
@@ -25,45 +79,45 @@ void merge(int arr[], int l, int m, int r) {
 //          (2) 'size' tells us how big the array of data is we are sorting.
 // Output: No value is returned, but 'array' should be modified to store a sorted array of numbers.
 void sortIntegers(int* array, unsigned int size){
-    // TODO: make a call to your mergeSort function here
+	mergeSort(array, 0, size-1); 
 }
 
 
 // Input: A pointer to an array (i.e. the array itself points to the first index)
 //        The size of the array (Because we do not know how big the array is automatically)
 void printIntArray(int* array, unsigned int size){
-  unsigned int i; // Note: 'unsigned int' is a datatype for storing positive integers.
-  for(i = 0; i < size; i=i+1){
-    printf("%d ",array[i]);
-  }
-  printf("\n");
+	unsigned int i; // Note: 'unsigned int' is a datatype for storing positive integers.
+	for(i = 0; i < size; i=i+1){
+		printf("%d ",array[i]);
+	}
+	printf("\n");
 }
 
 int main(){
   
-  // Some test data sets.
-  int dataset1[] = {0,1,2,3,4,5,6,7,8,9,10};
-  int dataset2[] = {10,9,8,7,6,5,4,3,2,1,0};
-  int dataset3[] = {0,3,2,1,4,7,6,5,8,9,10};
-  int dataset4[] = {2,1,1,1,1,1,1,1,1,1,1};
-  int dataset5[] = {100,201,52,3223,24,55,623,75,8523,-9,150};
-  int dataset6[] = {-1,1,2,-3,4,5,-6,7,8,-9,10};
+	// Some test data sets.
+	int dataset1[] = {0,1,2,3,4,5,6,7,8,9,10};
+	int dataset2[] = {10,9,8,7,6,5,4,3,2,1,0};
+	int dataset3[] = {0,3,2,1,4,7,6,5,8,9,10};
+	int dataset4[] = {2,1,1,1,1,1,1,1,1,1,1};
+	int dataset5[] = {100,201,52,3223,24,55,623,75,8523,-9,150};
+	int dataset6[] = {-1,1,2,-3,4,5,-6,7,8,-9,10};
   
-  // Sort our integer array
-  sortIntegers(dataset1, 11);
-  sortIntegers(dataset2, 11);
-  sortIntegers(dataset3, 11);
-  sortIntegers(dataset4, 11);
-  sortIntegers(dataset5, 11);
-  sortIntegers(dataset6, 11);
+	// Sort our integer array
+	sortIntegers(dataset1, 11);
+	sortIntegers(dataset2, 11);
+	sortIntegers(dataset3, 11);
+	sortIntegers(dataset4, 11);
+	sortIntegers(dataset5, 11);
+	sortIntegers(dataset6, 11);
   
-  // Print out an array
-  printIntArray(dataset1, 11);
-  printIntArray(dataset2, 11);
-  printIntArray(dataset3, 11);
-  printIntArray(dataset4, 11);
-  printIntArray(dataset5, 11);
-  printIntArray(dataset6, 11);
+	// Print out an array
+	printIntArray(dataset1, 11);
+	printIntArray(dataset2, 11);
+	printIntArray(dataset3, 11);
+	printIntArray(dataset4, 11);
+	printIntArray(dataset5, 11);
+	printIntArray(dataset6, 11);
   
-  return 0;
+	return 0;
 }
